ea1ec2edd1f7001f5a0c88d0ed743fafa0b71524
adrimarco <adrianmarco-@hotmail.es>
2025-02-02 13:55:29 +0100

    feat: add player movement

--- Assets/Scripts/PlayerComponent.cs
+++ Assets/Scripts/PlayerComponent.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerComponent : MonoBehaviour
 {
+    // Components
     private InputComponent inputComponent;
     private Rigidbody2D body;
+
+    // Movement atributes
+    const float MOV_ACCELERATION = 20f;
+    const float STOP_ACCELERATION = 40f;
+    const float MAX_SPEED = 5f;
+    private float currentSpeed = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,22 @@ public class PlayerComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        body.velocity = inputComponent.movementDirection;
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        bool isMoving = inputComponent.movementDirection != Vector2.zero;
+
+        if (isMoving)
+        {
+            currentSpeed = Mathf.Min(currentSpeed + MOV_ACCELERATION * Time.deltaTime, MAX_SPEED);
+            body.velocity = inputComponent.movementDirection * currentSpeed;
+        }
+        else
+        {
+            currentSpeed = Mathf.Max(currentSpeed - STOP_ACCELERATION * Time.deltaTime, 0f);
+            body.velocity = body.velocity.normalized * currentSpeed;
+        }
     }
 }
