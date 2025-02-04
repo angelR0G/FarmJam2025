4c6df93e4e306581351a62322a1bbed7488154f8
angelR0G <angelroblesgonzalez80@gmail.com>
2025-02-04 19:01:51 +0100

    feat:add night/day cicle

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(Light2D))]
+public class GameManager : MonoBehaviour
+{
+
+    public event EventHandler<TimeSpan> worldTimeChanged;
+    public event EventHandler<int> dayChanged;
+    public event EventHandler<double> hourChanged;
+
+    public const int MinutesInDay = 1440;
+
+    [SerializeField]
+    private float dayLength;
+
+    private TimeSpan currentTime;
+    private float minuteLenght => dayLength / MinutesInDay;
+    [SerializeField]
+    private Gradient gradient;
+
+    private Light2D light;
+
+    [SerializeField]
+    private int numDays;
+
+    private void Awake()
+    {
+        light = GetComponent<Light2D>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(AddMinute());
+    }
+    private IEnumerator AddMinute()
+    {
+        currentTime += TimeSpan.FromMinutes(1);
+        worldTimeChanged?.Invoke(this, currentTime);
+        light.color = gradient.Evaluate(PercentOfDay());
+        if(currentTime.ToString(@"mm")=="00")
+        {
+            hourChanged?.Invoke(this, currentTime.TotalHours);
+        }
+        
+        if (PercentOfDay() == 0) 
+        {
+            numDays++;
+            dayChanged?.Invoke(this, numDays);
+        }
+        yield return new WaitForSeconds(minuteLenght);
+        StartCoroutine(AddMinute());
+    }
+
+    private float PercentOfDay() { 
+        return (float)currentTime.TotalMinutes % MinutesInDay / MinutesInDay;
+    }
+}
---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeDisplay : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameManager gameManager;
+    private TMP_Text text;
+    [SerializeField]
+    private TMP_Text textDay;
+
+
+    private void Awake()
+    {
+        gameManager.worldTimeChanged += OnWorldTimeChanged;
+        gameManager.dayChanged += OnDayChanged;
+        gameManager.hourChanged += OnHourChanged;
+        text = GetComponent<TMP_Text>();
+    }
+    private void OnWorldTimeChanged(object sender, TimeSpan newTime)
+    {
+        text.SetText(newTime.ToString(@"hh\:mm"));
+    }
+    private void OnDayChanged(object sender, int newDay)
+    {
+        textDay.SetText(newDay.ToString());
+    }
+    private void OnHourChanged(object sender, double newHour)
+    {
+        Debug.Log("Hora + " + newHour);
+    }
+}
